cde94394edd74c4ec940f87e6ce9fb864bc5250b
DaeWon9 <qkreodnjs97@naver.com>
2022-09-23 00:50:39 +0900

    [REFACTOR] Appconfig refactor

--- demo/src/main/java/study/demo/AppConfig.java
+++ demo/src/main/java/study/demo/AppConfig.java
@@ -1,6 +1,8 @@
 package study.demo;
 
+import study.demo.discount.DiscountPolicy;
 import study.demo.discount.FixDiscountPolicy;
+import study.demo.member.MemberRepository;
 import study.demo.member.MemberService;
 import study.demo.member.MemberServiceImpl;
 import study.demo.member.MemoryMemberRepository;
@@ -10,11 +12,19 @@ import study.demo.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    private DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 
 }
